3d2c4b06d799942e5abd01a32ad32cee4a0309c1
trololzen <trololzen@gmail.com>
2022-03-10 17:03:27 +0100

    minor fixes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -15,7 +12,7 @@ import java.util.Random;
 
 public class Main {
     /////////////////// list of messages //////////////////
-    public static List<String> messagesList = Arrays.asList("a","b","c","d","e");
+    public static List<String> messagesList = new ArrayList<>();
     ///////////////////////////////////////////////////////
     public static  Random rand = new Random();
     public static String randomMessage(){
@@ -23,6 +20,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        StartWindow.show();
         System.setProperty("webdriver.chrome.driver","src/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
@@ -30,7 +28,8 @@ public class Main {
         System.out.println("*opening telegram page");
         driver.get("https://web.telegram.org/z/");
         System.out.println("*please log in, open any chat, and open list of members.");
-        WebDriverWait wait = new WebDriverWait(driver,55555);
+        WebDriverWait wait = new WebDriverWait(driver,60);
+        WebDriverWait wait5 = new WebDriverWait(driver,5);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.className("members-list")));
         System.out.println("*members list detected");
         WebElement membersListElement;
@@ -40,24 +39,53 @@ public class Main {
         do{
             membersListElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("members-list")));
             membersList = membersListElement.findElements(By.className("ListItem"));
-            membersList.remove(0);
+            //membersList.remove(0);
             System.out.println("opening chat with member");
             try {
                 membersList.get(i).click(); // possible exception here
                 System.out.println("waiting for phone icon");
-                wait.until(ExpectedConditions.presenceOfElementLocated(By.className("icon-phone")));
+                try {
+                    wait5.until(ExpectedConditions.presenceOfElementLocated(By.className("icon-phone")));
+                }
+                catch(TimeoutException e){
+                    driver.findElement(By.className("back-button")).click();
+                    i++;
+                    continue;
+                }
                 temp = driver.findElements(By.id("editable-message-text")).get(driver.findElements(By.id("editable-message-text")).size() - 1);
                 temp.sendKeys(randomMessage());
                 wait.until(ExpectedConditions.presenceOfElementLocated(By.className("send"))).click();
-                //driver.findElements(By.className("icon-send")).get(driver.findElements(By.className("icon-send")).size()-1).click();
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.className("record")));
                 System.out.println("sent!");
                 driver.findElement(By.className("back-button")).click();
             }
             catch(StaleElementReferenceException e){
-                System.out.println("exception was caught, moving to the next member");
+                System.out.println("StaleElementReferenceException was caught, moving to the next member");
+                if(driver.findElements(By.className("back-button")).size()!=0){
+                    driver.findElement(By.className("back-button")).click();
+                }
+            }
+            catch(ElementClickInterceptedException e){
+                System.out.println("ElementClickInterceptedException was caught, moving to the next member");
+                if(driver.findElements(By.className("back-button")).size()!=0){
+                    driver.findElement(By.className("back-button")).click();
+                }
+            }
+            catch(ElementNotInteractableException e){
+                System.out.println("ElementNotInteractableExceptionpo was caught, moving to the next member");
+                try {
+                    if (driver.findElements(By.className("back-button")).size() != 0) {
+                        driver.findElement(By.className("back-button")).click();
+                    }
+                }
+                catch(ElementNotInteractableException ee){
+                    driver.findElement(By.className("unread-count")).click();
+                }
             }
             i++;
         }
         while(i<membersList.size());
+        System.out.println("Reached the end. Exiting.");
+        System.exit(0);
     }
 }
